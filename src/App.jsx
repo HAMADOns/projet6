5298c37109836e46203bc9683f5505b64cb90f26
Ons HAMAD <onshamadkl@gmail.com>
2024-04-02 00:24:07 +0100

    FAQ

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,17 @@ function App() {
         <div className="header"></div>
         <div className="content">
           <div className="bloc">
+            <img
+              src="../public/etoile.png"
+              alt="Description de l'image"
+              style={{
+                width: "40px",
+                float: "left",
+                marginRight: "10px",
+                marginLeft: "10px",
+                marginTop: "25px",
+              }}
+            />
             <h1
               style={{ color: "purple", textAlign: "left", marginLeft: "30px" }}
             >
